df9b7f39fcecec75fe4434273e24b52000e5cc36
Adam <97778776+ItsGoldeneyes@users.noreply.github.com>
2022-11-20 23:38:34 -0500

    e

--- src/main.py
+++ src/main.py
@@ -2,12 +2,14 @@ from othello import OthelloGame
 
 def main():
     game = OthelloGame()
-    # game.show()
-    # while not game.winner:
-    #     # game.turn()
-    #     game.show()
-    game.turn("X")
     game.show()
+    player = 0
+    while not game.winner:
+        game.turn(player+1)
+        game.show()
+        
+        player = (player + 1)%2
+    
+    
 if __name__ == "__main__": 
     main()
-
--- src/othello.py
+++ src/othello.py
@@ -12,6 +12,10 @@ class OthelloGame():
     
     
     def extract_pos(self, pos_str):
+        '''
+        Get position from input + flip so orients correctly
+        '''
+        
         first = re.search(r"\d", pos_str)
         if first == None:
             raise ValueError("Input contains no numbers")
@@ -20,16 +24,14 @@ class OthelloGame():
         if second == None:
             raise ValueError("Input contains only one number")
         
-        return (int(first.group()), int(second.group()))
-    
-    
-    def check_move(self, player, move):
-        return True
+        return (int(second.group())-1, int(first.group())-1)
         
         
     def turn(self, player, pos= False):
+        assert player == 1 or player == 2
+        
         if pos:
-            if not self.check_move(player, pos):
+            if not self.board.check_move(player, pos):
                 return False
             else:
                 self.board.move(player, pos)
@@ -47,29 +49,80 @@ class OthelloGame():
                     print(e)
                     
                 if pos_tuple:
-                    valid = self.check_move(player, pos_tuple)
+                    valid = self.board.check_move(player, pos_tuple)
                     
             self.board.move(player, pos_tuple)
                 
     
     
 class OthelloBoard():
-    def __init__(self):
-        self.board = [[0 for i in range(8)] for j in range(8)]
+    def __init__(self, size=8):
+        self.board = [[0 for __ in range(size)] for _ in range(size)]
+        
+        self.board[size//2][size//2] = 1
+        self.board[(size//2)-1][(size//2)-1] = 1
+        self.board[(size//2)-1][size//2] = 2
+        self.board[size//2][(size//2)-1] = 2
+    
         
     def move(self, player, pos):
-        assert player == "X" or player == "O", "Player must be either X or O"
         assert type(pos) == tuple and len(pos) == 2, "Position must be tuple of length 2"
         assert 0 <= pos[0] <= 7, "X position out of bounds (0<x<7)"
         assert 0 <= pos[1] <= 7, "Y position out of bounds (0<x<7)"
         
-        self.board[pos[0]][pos[1]] = player
+        self.board[pos[0]][pos[1]] = player+1
+        
+        
+    def check_move(self, player, move):
+        print(move)
+        if move[0] > 7 or move[0] < 0:
+            print("X is out of bounds")
+            return False
+        if move[1] > 7 or move[1] < 0:
+            print("Y is out of bounds")
+            return False
+        
+        if self.board[move[0]][move[1]] != 0:
+            print("space is taken")
+            return False
+        
+        adjacent_to_enemy = False
+        
+        if move[0] <= len(self.board[0]):
+            if self.board[move[0]+1][move[1]] == ((player+1)%2)+1:
+                adjacent_to_enemy = True
+                
+        if move[1] <= len(self.board[0]):
+            if self.board[move[0]][move[1]+1] == ((player+1)%2)+1:
+                adjacent_to_enemy = True
+                
+        if move[0] >= 0:
+            if self.board[move[0]-1][move[1]] == ((player+1)%2)+1:
+                adjacent_to_enemy = True
+                
+        if move[1] >= 0:
+            if self.board[move[0]][move[1]-1] == ((player+1)%2)+1:
+                adjacent_to_enemy = True
+        
+        if not adjacent_to_enemy:
+            print("Not adjacent to enemy")
+            return False
+            
+        return True
         
     def __str__(self):
         return_val = ""
-        for line in self.board:
+        add_val = ""
+        for line in self.board[::-1]:
             for square in line:
-                return_val += " " + str(square)
+                if square == 0:
+                    add_val = '\u2218'
+                elif square%2 == 1: 
+                    add_val = '\u25EF'
+                elif square%2 == 0: 
+                    add_val = '\u2B24'
+                    
+                return_val += " " + add_val
             return_val += "\n"
             
         return return_val
