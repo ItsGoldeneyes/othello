b520784a4552140f80c0d25c9f77dd35336a129d
Adam <97778776+ItsGoldeneyes@users.noreply.github.com>
2022-12-25 01:49:40 -0500

    Evaluation changes

--- src/othello_minimax.py
+++ src/othello_minimax.py
@@ -13,16 +13,47 @@ class Minimax:
         
         best_move = [(0, 0), -(math.inf)]
         for move in board.get_actual_moves(player):
-            _, val = self.minimax(board, player, depth=4)
+            _, val = self.minimax(board, player, depth=3)
             
             if val > best_move[1]:
                 best_move = [move, val]
         return best_move[0]
     
-    
-    def evaluate(self, board):        
-        score = 0
+
+    def evaluate_piece_count(self, board):
+        piece_count = board.count_pieces(self.player)
+        opp_piece_count = board.count_pieces((self.player%2)+1)
         
+        return 100*(piece_count - opp_piece_count)/(piece_count + opp_piece_count)
+        
+    
+    def evaluate_mobility(self, board):
+        mobility = len(board.get_potential_moves(self.player))
+        opp_mobility = len(board.get_potential_moves((self.player%2)+1))
+        
+        if (mobility + opp_mobility) == 0:
+            return 0
+        
+        return 100*(mobility - opp_mobility)/(mobility + opp_mobility)
+        
+        
+    def evaluate_corners(self, board):
+        corner_count = board.board[0][0]==self.player + \
+                        board.board[0][7]==self.player + \
+                        board.board[7][0]==self.player + \
+                        board.board[7][7]==self.player
+        opp_corner_count = board.board[0][0]==((self.player%2)+1) + \
+                        board.board[0][7]==((self.player%2)+1) + \
+                        board.board[7][0]==((self.player%2)+1) + \
+                        board.board[7][7]==((self.player%2)+1)
+                        
+        if (corner_count + opp_corner_count) == 0:
+            return 0
+        
+        return 100*(corner_count - opp_corner_count)/(corner_count + opp_corner_count)
+        
+        
+    def evaluate_static_weights(self, board):
         static_weights = [[4,  -3,  2,  2,  2,  2, -3,  4],
                           [-3, -4, -1, -1, -1, -1, -4, -3],
                           [2,  -1,  1,  0,  0,  1, -1,  2],
@@ -32,36 +63,33 @@ class Minimax:
                           [-3, -4, -1, -1, -1, -1, -4, -3],
                           [4,  -3,  2,  2,  2,  2, -3,  4]]
         
-        piece_locations = [[x if x == 1 else 0 for x in row ] for row in board]
+        piece_locations = [[static_weights[x][y] if board.board[x][y] == self.player else 0 
+                            for y in range(len(board.board)) ] 
+                           for x in range(len(board.board))]
+        opp_piece_locations = [[static_weights[x][y] if board.board[x][y] == (self.player%2)+1 else 0 
+                            for y in range(len(board.board)) ] 
+                           for x in range(len(board.board))]
         
-        piece_count = board.count_pieces(self.player)
-        opp_piece_count = board.count_pieces((self.player%2)+1)
+        total_weight = sum(map(sum, piece_locations))
+        opp_total_weight = sum(map(sum, opp_piece_locations))
         
-        corner_count = board.board[0][0]==self.player + \
-                        board.board[0][7]==self.player + \
-                        board.board[7][0]==self.player + \
-                        board.board[7][7]==self.player       
-        opp_corner_count = board.board[0][0]==((self.player%2)+1) + \
-                        board.board[0][7]==((self.player%2)+1) + \
-                        board.board[7][0]==((self.player%2)+1) + \
-                        board.board[7][7]==((self.player%2)+1)
-        
-        edges = [x[0] for x in board.board] + \
-                [x[7] for x in board.board] + \
-                board.board[0][1:6] + board.board[7][1:6]
-        edge_count = edges.count(self.player)
-        opp_edge_count = edges.count((self.player%2)+1)
-        
-        # safe_pieces = board.get_safe_pieces(self.player)
+        return total_weight-opp_total_weight
         
         
-        score += corner_count*5
-        score -= opp_corner_count*2
-        score += edge_count//2
-        score -= opp_edge_count//4
-        score += piece_count
+    def evaluate(self, board):
+        score = 0
         
-        return score #piece_count-opp_piece_count
+        piece_count = self.evaluate_piece_count(board)
+        mobility = self.evaluate_mobility(board)
+        corners = self.evaluate_corners(board)
+        static_weights = self.evaluate_static_weights(board)
+        
+        score += 0.5*piece_count
+        score += 2*mobility
+        score += 5*corners
+        score += 5*static_weights
+        
+        return score
         
         
     def minimax(self, board, player, depth=2, alpha=-math.inf, beta=math.inf):
